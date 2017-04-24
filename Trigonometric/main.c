15ab2b1f83a17571c6dbef7dd5f2fae1743b1a75
Niklas Sauer <nik.sauer@me.com>
2017-04-24 09:54:18 +0200

    Initial Commit

---
+++ Trigonometric/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Trigonometric
+//
+//  Created by Niklas Sauer on 24.04.17.
+//  Copyright © 2017 DHBW Stuttgart. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
